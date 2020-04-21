6cab39279c987f84dce94d977c560d74219f0b31
Mat Sz <contact@matsz.dev>
2020-04-21 19:52:10 +0200

    first commit

---
+++ build/rollup.config.js
@@ -0,0 +1,23 @@
+import commonjs from 'rollup-plugin-commonjs';
+import vue from 'rollup-plugin-vue';
+import buble from 'rollup-plugin-buble';
+import resolve from '@rollup/plugin-node-resolve';
+import ignore from 'rollup-plugin-ignore';
+
+export default {
+  input: 'src/wrapper.js',
+  output: {
+    name: 'Letter',
+    exports: 'named',
+  },
+  plugins: [
+    ignore('react'),
+    commonjs(),
+    vue({
+      css: true,
+      compileTemplate: true,
+    }),
+    resolve(),
+    buble(),
+  ],
+};
---
+++ src/Letter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-bind:class="[className]" v-html="sanitizedHtml" />
+</template>
+
+<script>
+import { sanitize } from 'react-letter';
+
+export default {
+  name: 'Letter',
+  props: {
+    className: String,
+    html: String,
+    text: String,
+  },
+  computed: {
+    sanitizedHtml: function () {
+      return sanitize(this.html, this.text);
+    },
+  },
+};
+</script>
