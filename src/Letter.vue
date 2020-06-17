fdacbd89aff1d516d1436811d263c054c12d50e6
Mat Sz <contact@matsz.dev>
2020-06-17 18:14:59 +0200

    added an useIframe attribute

--- src/Letter.vue
+++ src/Letter.vue
@@ -1,5 +1,6 @@
 <template>
-  <div v-bind:class="[className]" v-html="sanitizedHtml" />
+  <div v-if="!useIframe" v-bind:class="[className]" v-html="sanitizedHtml" />
+  <iframe v-if="useIframe" v-bind:class="[className]" v-bind:src="iframeSrc" />
 </template>
 
 <script>
@@ -11,10 +12,16 @@ export default {
     className: String,
     html: String,
     text: String,
+    useIframe: Boolean,
   },
   computed: {
     sanitizedHtml: function () {
       return sanitize(this.html, this.text);
+    },
+    iframeSrc: function () {
+      return (
+        'data:text/html;charset=utf-8,' + encodeURIComponent(this.sanitizedHtml)
+      );
     },
   },
 };
